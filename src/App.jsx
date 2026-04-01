0d24ceb6b9b557597298b1a480c1374286adb59c
Ziad Elsayyad <ziad.elsyad16@gmail.com>
2026-04-01 23:36:32 +0200

    feat: implement game status logic and dynamic styling for win/loss states

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,29 @@ import Keyboard from "./components/Keyboard";
 import LanguageTracker from "./components/LanguageTracker";
 import WordDisplay from "./components/WordDisplay";
 import NewGameButton from "./components/NewGameButton";
+import { languages } from "./languages";
 
 function App() {
   // Correct letter: display the letter in WordDisplay component & change the key color in keyboard
   // Wrong letter: kill a language & change the key color in keyboard
   const [guessedLetters, setGuessedLetters] = useState([]);
-  const [currentWord, setCurrentWord] = useState("react");
+  const [currentWord] = useState("react");
 
+  // COUNTING EACH WRONG PRESS
   const wrongGuessCount = guessedLetters.filter(
     (letter) => !currentWord.includes(letter),
   ).length;
 
+  // GAME STATUS --> playing | won | lost
+  const isGameWon = currentWord
+    .split("")
+    .every((letter) => guessedLetters.includes(letter));
+
+  const isGameLost = wrongGuessCount >= languages.length - 1 ? true : false;
+
+  const isGameOver = isGameLost || isGameWon;
+
+  // ADDING EACH LETTER THAT USER PRESSED ON KEYBOARD
   function addGuessedLetter(letter) {
     setGuessedLetters((prevLetters) =>
       prevLetters.includes(letter) ? prevLetters : [...prevLetters, letter],
@@ -25,7 +37,7 @@ function App() {
   return (
     <main>
       <Header />
-      <GameStatus />
+      {<GameStatus gameOver={isGameOver} gameWon={isGameWon} />}
       <LanguageTracker wrongGuessCount={wrongGuessCount} />
       <WordDisplay word={currentWord} guessedLetters={guessedLetters} />
       <Keyboard
@@ -33,7 +45,7 @@ function App() {
         word={currentWord}
         guessedLetters={guessedLetters}
       />
-      <NewGameButton />
+      {isGameOver && <NewGameButton />}
     </main>
   );
 }
--- src/components/GameStatus.jsx
+++ src/components/GameStatus.jsx
@@ -1,10 +1,27 @@
-const GameStatus = () => {
-  return (
-    <div className="game-status">
-      <h2>You Win!</h2>
-      <p>Well done! 🎉</p>
-    </div>
-  );
+const GameStatus = ({ gameOver, gameWon }) => {
+  function renderGameStatus() {
+    if (!gameOver) {
+      return null;
+    } else if (gameWon) {
+      return (
+        <>
+          <h2>You Win!</h2>
+          <p>Well done! 🎉</p>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <h2>Game over!</h2>
+          <p>You lose! Better start learning Assembly 😭</p>
+        </>
+      );
+    }
+  }
+
+  const className = `game-status ${gameOver ? (gameWon ? "won" : "lost") : ""}`;
+
+  return <div className={className}>{renderGameStatus()}</div>;
 };
 
 export default GameStatus;
